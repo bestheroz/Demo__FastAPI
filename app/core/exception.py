f510743a28da6a13274a6626f199757a286947df
joony.kim <bestheroz@gmail.com>
2025-09-12 10:43:05 +0900

    ♻️

--- app/core/exception.py
+++ app/core/exception.py
@@ -1,41 +1,49 @@
 from app.core.code import Code
 
 
-class BadRequestException400(Exception):
+class BaseException(Exception):
+    """Base exception class to reduce code duplication."""
+    
     def __init__(self, code: Code, data: dict | list | None = None):
         self.code = code.name
         self.message = code.value
         self.data = data
+        super().__init__(self.message)
 
 
-class UnauthorizedException401(Exception):
+class BadRequestException400(BaseException):
+    """400 Bad Request exception."""
+    pass
+
+
+class UnauthorizedException401(BaseException):
+    """401 Unauthorized exception."""
+    
     def __init__(
         self,
         code: Code = Code.UNKNOWN_AUTHENTICATION,
         data: dict | list | None = None,
     ):
-        self.code = code.name
-        self.message = code.value
-        self.data = data
+        super().__init__(code, data)
 
 
-class ForbiddenException403(Exception):
+class ForbiddenException403(BaseException):
+    """403 Forbidden exception."""
+    
     def __init__(
         self,
         code: Code = Code.UNKNOWN_AUTHORITY,
         data: dict | list | None = None,
     ):
-        self.code = code.name
-        self.message = code.value
-        self.data = data
+        super().__init__(code, data)
 
 
-class UnknownSystemException500(Exception):
+class UnknownSystemException500(BaseException):
+    """500 Internal Server Error exception."""
+    
     def __init__(
         self,
         code: Code = Code.UNKNOWN_SYSTEM_ERROR,
         data: dict | list | None = None,
     ):
-        self.code = code.name
-        self.message = code.value
-        self.data = data
+        super().__init__(code, data)
